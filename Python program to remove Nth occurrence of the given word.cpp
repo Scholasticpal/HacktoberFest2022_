4434468c5f50fd2064a9c6c8017bdf95e5884f19
HRNHarshit <115611593+HRNHarshit@users.noreply.github.com>
2022-10-12 15:52:23 +0530

    Create Python program to remove Nth occurrence of the given word.cpp

---
+++ Python program to remove Nth occurrence of the given word.cpp
@@ -0,0 +1,29 @@
+Python program to remove Nth occurrence of the given word
+
+//code
+
+def RemoveIthWord(lst, word, N):
+	newList = []
+	count = 0
+	for i in lst:
+		if(i == word):
+			count = count + 1
+			if(count != N):
+				newList.append(i)
+		else:
+			newList.append(i)
+			
+	lst = newList
+	
+	if count == 0:
+		print("Item not found")
+	else:
+		print("Updated list is: ", lst)
+	
+	return newList
+
+list = ["geeks", "for", "geeks"]
+word = "geeks"
+N = 2
+
+RemoveIthWord(list, word, N)
