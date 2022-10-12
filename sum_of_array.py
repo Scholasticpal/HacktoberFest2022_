b865ff631a09147243d64a21ed36a5eac81c9436
Aditya Garg <115340113+AG-444@users.noreply.github.com>
2022-10-12 19:51:23 +0530

    Add files via upload

---
+++ sum_of_array.py
@@ -0,0 +1,19 @@
+list1 = []
+i = 0
+a = 0
+print("Input numbers and input @ to stop inputing")
+while i < 1:
+    x = input()
+    if x == "@":
+        break
+    elif x.isdigit():
+        list1.append(int(x))
+    else:
+        print("INVALID INPUT")
+print("array is:", list1)
+
+for i in range(0, len(list1)):
+    a += list1[i]    
+
+print("Sum of all the elements of an array: " + str(a))
+
