1fa65a798a5f3a9dcda9d464829f0f6cd9451689
Aryan <Aryansinghchandel1404@gmail.com>
2022-10-12 15:43:38 +0530

    sum of array added

---
+++ sumofarray.py
@@ -0,0 +1,9 @@
+#Initialize array     
+arr = [1, 2, 3, 4, 5];     
+sum = 0;    
+     
+#Loop through the array to calculate sum of elements    
+for i in range(0, len(arr)):    
+   sum = sum + arr[i];    
+     
+print("Sum of all the elements of an array: " + str(sum));    
