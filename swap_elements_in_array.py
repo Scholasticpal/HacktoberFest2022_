c9179a6c1e4a904eeef574bd0f025147b64f52b6
Aditya Garg <115340113+AG-444@users.noreply.github.com>
2022-10-12 22:39:11 +0530

    Update swap_elements_in_array.py

--- swap_elements_in_array.py
+++ swap_elements_in_array.py
@@ -17,7 +17,7 @@ print("select two positions to be swapped")
 n = int(input("first position: "))
 m = int(input("Second position: "))
 
-if n < len(list1) and m < len(list1):
+if n =< len(list1) and m =< len(list1):
     list1[n-1],list1[m-1] = list1[m-1], list1[n-1]
     print(list1)
 else:
