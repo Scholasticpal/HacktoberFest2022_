63e66408b9da9863e4e11d6e08549d3662c70ab0
Aditya Garg <115340113+AG-444@users.noreply.github.com>
2022-10-14 15:32:23 +0530

    Update swap-elements-in-array.py

--- swap-elements-in-array.py
+++ swap-elements-in-array.py
@@ -26,13 +26,13 @@ while i < 1:
     else:
         list1.append(x)
 #swap elements of list
-print("the array is:"list1)
+print(list1)
 print("select two positions to be swapped")
 n = int(input("first position: "))
 m = int(input("Second position: "))
 
 if n <= len(list1) and m <= len(list1):
     list1[n-1],list1[m-1] = list1[m-1], list1[n-1]
-    print("the new array is:",list1)
+    print("the new list is:",list1)
 else:
     print("INVALID POSITIONS")
